99db5e6a85ed2330fcb1e1f43f22c61646de5354
Mitar <mitar.git@tnode.com>
2012-10-30 03:24:43 -0700

    Template tags.

--- mongo_socialauth/templatetags/mongo_socialauth.py
+++ mongo_socialauth/templatetags/mongo_socialauth.py
@@ -5,8 +5,11 @@ register = template.Library()
 @register.inclusion_tag('user/user_image.html', takes_context=True)
 def user_image(context, user=None):
     if user is None:
-        user = context['user']
+        user = context.get('user')
 
-    return {
-        'user_image_url': user.get_image_url(),
-    }
+    if user:
+        return {
+            'user_image_url': user.get_image_url(),
+        }
+
+    return {}
